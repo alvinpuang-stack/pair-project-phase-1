6dd934e898ce67c05a7e6332e6e2a21a14972984
alvinpuang-stack <alvin.puang98@gmail.com>
2024-01-25 09:16:20 +0700

    pair project

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+npx sequelize-cli model:generate --name User --attributes firstName:string,lastName:string,email:string
+npx sequelize-cli model:generate --name Course --attributes firstName:string,lastName:string,email:string
+npx sequelize-cli model:generate --name UserProfile --attributes firstName:string,lastName:string,email:string
+npx sequelize-cli model:generate --name Enrollment --attributes firstName:string,lastName:string,email:string
+npx sequelize-cli model:generate --name UserCourse --attributes firstName:string,lastName:string,email:string
+*/
+
